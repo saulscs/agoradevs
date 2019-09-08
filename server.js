0b40b4e301c7ddb7666fa026253807da322ecfed
saulscs <saulcoronasantos@ymail.com>
2019-09-08 17:19:25 -0500

    conection to database

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db.js')
 
 const app = express();
+
+// Connect Database
+
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running '));
 
