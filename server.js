4ce1438e818d9c6c6409db8256ed430a2b95acdb
saulscs <saulcoronasantos@ymail.com>
2019-09-08 18:46:17 -0500

    D1 Route files with express router

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const connectDB = require('./config/db.js')
 
 const app = express();
 
-// Connect Database
+//Connect Database
 
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running '));
+
+//Define Routes 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
 
 const PORT = process.env.PORT || 5000;
 
