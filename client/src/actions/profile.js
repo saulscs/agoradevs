e5bac612c68866f1a801e6d09123fdb6ac069caf
saulscs <saulcoronasantos@ymail.com>
2020-02-25 22:30:05 -0600

    Addresing the console warning

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -221,7 +221,7 @@ export const deleteAccount = () => async dispatch =>  {
 
     if(window.confirm('Are you sure? This can NOT be undon')){
         try {
-            const res = await axios.delete(`/api/profile`)
+            await axios.delete(`/api/profile`)
             
             dispatch({
                 type: CLEAR_PROFILE
