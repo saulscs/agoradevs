def9fe5b3d6de87c5558ffb6fe16b5013f36cc4e
saulscs <saulcoronasantos@ymail.com>
2020-02-26 11:57:46 -0600

    Startingon the profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -47,9 +47,9 @@ export const getProfiles = () => async dispatch => {
 
 export const getProfileById = userId => async dispatch => {
     try {
-        const res = await axios.get(`/api/profile/${userId}`);
+        const res = await axios.get(`/api/profile/user/${userId}`);
         dispatch({
-            type: GET_PROFILES,
+            type: GET_PROFILE,
             payload: res.data
         });
     } catch (err) {
