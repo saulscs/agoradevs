48db54da2a37e467eada2c6e32df066462f8b206
saulscs <saulcoronasantos@ymail.com>
2020-03-01 19:40:35 -0600

    Post reducer, Action & Initial component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import EditProfile from './components/profile-form/EditProfile';
 import AddExperience from './components/profile-form/AddExperience';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/post/Posts';
 import AddEducation from './components/profile-form/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -52,6 +53,7 @@ const App = ()  => {
            <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
            <PrivateRoute exact path="/add-experience" component={AddExperience}/>
            <PrivateRoute exact path="/add-education" component={AddEducation}/>
+           <PrivateRoute exact path="/posts" component={Posts}/>
          </Switch>
        </section>
      </Fragment>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -16,7 +16,9 @@ export const getCurrentProfile = () => async dispatch => {
             type: GET_PROFILE,
             payload: res.data
         });
+        
     } catch (err) {
+        dispatch({ type: CLEAR_PROFILE });
         dispatch({
             type: PROFILE_ERROR,
             payload: {msg: err.response.statusText, status: err.response.status}
